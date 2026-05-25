44f6051ba2f90a60d445b7ab046e05cae312b4f3
ValentinRigoni <rigonitomas@outlook.com>
2026-05-25 20:26:41 -0300

    chore: refresh frontend dist build

--- distFront/.next/static/chunks/app/dashboard/layout-63af3927f70f0cf3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7663,9248,485,9786,4095],{2707:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,7397))}},function(e){e.O(0,[763,2972,5039,5878,361,7397,2971,2117,1744],function(){return e(e.s=2707)}),_N_E=e.O()}]);
