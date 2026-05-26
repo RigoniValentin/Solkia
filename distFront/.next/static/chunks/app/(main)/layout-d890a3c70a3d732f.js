6d2be91e20cd6b279714e3c767bcd6360c5ff8f1
ValentinRigoni <rigonitomas@outlook.com>
2026-05-25 21:52:00 -0300

    Update

---
+++ distFront/.next/static/chunks/app/(main)/layout-d890a3c70a3d732f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4095,9248,9786,485,7663],{2707:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,7397))}},function(e){e.O(0,[763,2972,5039,5878,361,7397,2971,2117,1744],function(){return e(e.s=2707)}),_N_E=e.O()}]);
